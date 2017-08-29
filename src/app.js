b3fdebea564312d7928626f3437752ca5f0b4061
Horácio Dias <horacio.dias92@gmail.com>
2017-08-29 00:15:59 -0300

    Atualizando os paranaues para redesenhar e redimensionar o canvas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-const template = `<section class="container">
-    <header class="header">
-      <nav>
-        <h1>Simbologia {{ $route.params.id }}</h1>
-        <menu-items></menu-items>
-      </nav>
-    </header>
-  </section>
+const template = `
+<v-layout v-resize="onResize" column align-center justify-center>
+   <v-subheader>Simbologia {{ $route.params.id }}</v-subheader>
+   <menu-items ref="menu" :width="width" :height="height"></menu-items>
+   {{ windowSize }}
+ </v-layout>
 `
 import menuItems from './components/menu/index'
 
@@ -14,11 +12,31 @@ export default {
   components: {
     menuItems
   },
-  data () {
-    return {}
+  computed:{
+    width: function () {
+      return this.windowSize.x
+    },
+    height: function () {
+      return this.windowSize.y
+    }
   },
-  beforeMount () {
-
+  data () {
+    return {
+      windowSize: {
+        x: 600,
+        y: 300
+      }
+    }
+  },
+  methods: {
+    onResize () {
+      this.windowSize = { x: window.innerWidth, y: window.innerHeight }
+      let menu = this.$refs.menu
+      menu.updateCanvas()
+    }
+  },
+  mounted() {
+    this.onResize()
   },
   template: template
 }
--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -4,7 +4,7 @@ const template = `<nav class="container">
         <a :title="item.label" class="nav button "><i class="fa fa-cogs"></i> {{ item.label }}</a>
       </li>
     </ul>
-    <canvas width="600" height="300" id="canvas"></canvas>
+    <div ref="groupCanvas"></div>
   </nav>
 `
 import { menuItems } from '../../models/index'
@@ -12,24 +12,40 @@ import fabric from 'fabric'
 
 export default {
   name: 'menu',
+  props: ['width', 'height'],
   components: {
   },
   data () {
     return {
-      items:menuItems
+      items: menuItems
+    }
+  },
+  computed: {
+    myWidth: function() {
+      return this.width
+    },
+    myHeight: function() {
+      return this.height
+    }
+  },
+  methods: {
+    updateCanvas: function () {
+      console.log('update')
+      this.$refs.groupCanvas.innerHTML = ''
+      this.$refs.groupCanvas.innerHTML = `<canvas id="canvas" width="${this.myWidth}" height="${this.myHeight}">`
+      let canvas = new fabric.Canvas('canvas')
+      let rect = new fabric.Rect({
+        top : 100,
+        left : 100,
+        width : 60,
+        height : 70,
+        fill : 'red',
+        selectable:false
+      })
+      canvas.add(rect)
     }
   },
   mounted(){
-    let canvas = new fabric.Canvas('canvas')
-    let rect = new fabric.Rect({
-      top : 100,
-      left : 100,
-      width : 60,
-      height : 70,
-      fill : 'red',
-      selectable:false
-    })
-    canvas.add(rect)
   },
   template: template
 }
